7fa107d2aa7fbfef51c5479a4f91e0f5ffb06392
rvsmooth <riveeks.smooth@gmail.com>
2025-10-03 07:52:22 +0530

    setup shell

--- setup.sh
+++ setup.sh
@@ -1,13 +1,81 @@
 #!/bin/bash
+# Color Codes
+RESET='\033[0m'
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+
+# Variables
 TERM_DIR="${HOME}/.termux"
+DEF_FONT="${TERM_DIR}/font.ttf"
+# Color Functions
+PRED() {
+  echo -e "${RED}=> $@${RESET}"
+}
+
+PGREEN() {
+  echo -e "${GREEN}=> $@${RESET}"
+}
+
+PYELL() {
+  echo -e "${YELLOW}=> $@${RESET}"
+}
+
+PBLUE() {
+  echo -e "${BLUE}=> $@${RESET}"
+}
+
+PMAG() {
+  echo -e "${MAGENTA}=> $@${RESET}"
+}
+
+PCYAN() {
+  echo -e "${CYAN}=> $@${RESET}"
+}
+
+ewhite() {
+  echo -e "${WHITE}=> $@${RESET}"
+}
+
+PDONE() {
+  sleep 1 && PGREEN Done... && echo && sleep 1
+}
+
+PYELL Updating packages
 # update packages
 pkg update && pkg upgrade -y
+PDONE
 
+PYELL Installing dependencies
 # install dependencies
 pkg install -y wget curl
+PDONE
 
-# get font package
-wget -O $TERM_DIR/font.ttf https://github.com/rvsmooth/termux-setup/raw/refs/heads/master/fonts/JetBrainsMonoNerdFontMono-Medium.ttf
+PYELL Removing old font if exists
+[ -e "$DEF_FONT" ] && rm "$DEF_FONT"
+PDONE
+
+PYELL Installing jetbrainsmono font
+wget -qO "$DEF_FONT" https://github.com/rvsmooth/termux-setup/raw/refs/heads/master/fonts/JetBrainsMonoNerdFontMono-Medium.ttf
+PDONE
+
+PYELL Installing other packages
+pkg install -y platform-tools starship fish
+PDONE
+
+PYELL Setting up shell
+mkdir "$TERM_DIR"/.config
+mkdir "$TERM_DIR"/.config/fish
+wget -qO "$TERM_DIR"/.config/starship.toml https://raw.githubusercontent.com/rvsmooth/dotfiles/refs/heads/main/.config/starship.toml
+echo 'eval "$(starship init bash)"' >~/.bashrc
+echo 'starship init fish | source' >~/.config/fish/config.fish
+chsh -s fish
+PDONE
 
 # reload termux
 am broadcast --user 0 -a com.termux.app.reload_style com.termux

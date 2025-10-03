73cb04d453cdccf2a227ed26ae59895dd6272d1f
rvsmooth <riveeks.smooth@gmail.com>
2025-10-03 07:39:52 +0530

    fix font url

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ pkg update && pkg upgrade -y
 pkg install -y wget curl
 
 # get font package
-wget -O $TERM_DIR/font.ttf https://github.com/rvsmooth/termux-setup/blob/master/fonts/JetBrainsMonoNerdFontMono-Medium.ttf
+wget -O $TERM_DIR/font.ttf https://github.com/rvsmooth/termux-setup/raw/refs/heads/master/fonts/JetBrainsMonoNerdFontMono-Medium.ttf
 
 # reload termux
 am broadcast --user 0 -a com.termux.app.reload_style com.termux
